47db9f66b662cdbc55fc2866f2a40f59e2ffbda0
hiroka <mementoka@gmail.com>
2023-04-16 23:40:08 +0900

    Get Introduction

--- index.js
+++ index.js
@@ -2,18 +2,51 @@ const pdf = require('pdf-parse');
 const fs = require('fs');
 
 const dataBuffer = fs.readFileSync('sensors_oka_v2_en.pdf');
-const keywords = ['Abstract', 'Introduction', 'Related Works'];
+const sectionKeywords = ['Abstract', 'Introduction', 'Related Works'];
+const ignoreKeywords = ['Version April 3'];
 
 pdf(dataBuffer).then((data) => {
   const text = data.text;
   const splittedText = text.split('\n');
 
-  const numExp = /\d/;
-  for (const line of splittedText) {
-    if (line.includes(keywords[0])) {
-      console.log(line);
+  const numExp = /\d/g;
+  let printFlag = false;
+  for (let line of splittedText) {
+    if (line.includes(ignoreKeywords[0])) {
+      continue;
     }
+
+    if (line.includes(sectionKeywords[1])) {
+      printFlag = true;
+    }
+
+
+    if (line.match(numExp)) {
+      const digitCaptureGroup = [...line.matchAll(numExp)];
+      const digitIndices = digitCaptureGroup.map(cg => cg.index);
+      const ignoreIndices = [line.length -1,line.length - 2, line.length - 3];
+      const digitIgnoreIndices = digitIndices.filter(di => ignoreIndices.includes(di));
+      
+      for (const digitIndex of digitIndices) {
+        if (ignoreIndices.includes(digitIndex)) {
+          line = line.substring(0, digitIndex);
+        }
+      }
+      digitIndices.forEach((digitIndex, i) => {
+        if (ignoreIndices.includes(digitIndex)) {
+          line = line.substring(0, digitIndex+i);
+        }
+      });
+    }  
+
+    if (line.includes(sectionKeywords[2])) {
+      printFlag = false;
+    }
+
+    if (printFlag) console.log(line);
+
   }
+
     //console.log(splittedText);
   //fs.writeFileSync('./pdf.txt', text);
 }).catch((err) => {
